0ef7295e2262d6df83f6160b70f7438a3888c6c3
zackcreach <zackcreach@gmail.com>
2017-11-26 20:02:36 -0500

    Images up and running, still working on animations.

--- src/components/transition.js
+++ src/components/transition.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { CSSTransition } from 'react-transition-group';
 
-const Transition = ({ shouldShow, timeout, classNames, children }) => (
+const Transition = ({ shouldShow, timeout, classNames, children, ...props }) => (
   <CSSTransition
+    {...props}
     timeout={timeout}
     classNames={classNames}
     in={shouldShow}
--- src/components/viewer.js
+++ src/components/viewer.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Link from 'gatsby-link';
 import Img from 'gatsby-image';
-import { CSSTransitionGroup } from 'react-transition-group';
 
-import { next, back } from '../helpers/carousel';
+// import { next, back } from '../helpers/carousel';
+import { TransitionGroup } from 'react-transition-group';
+import Transition from '../components/transition';
 
 import '../styles/styles.scss';
 import styled, { css } from 'emotion';
@@ -12,18 +13,43 @@ class Viewer extends React.Component {
   constructor({ imageData }) {
     super();
     this.state = {
-      currentImage: imageData[1]
+      gallery: Object.values(imageData),
+      currentImageIndex: 0,
+      in: false
     }
   }
   componentWillMount() {
-    console.log(this.state.currentImage);
+    // setInterval(() => this.changeImage(), 2000);
+  }
+  componentWillUnmount() {
+    this.setState({
+      in: false
+    })
+  }
+  renderImage() {
+    const { gallery, currentImageIndex } = this.state;
+    return (
+      <Transition 
+        timeout={300}
+        classNames='slide'
+        shouldShow={!this.state.in}
+      >
+        <Img sizes={gallery[currentImageIndex].sizes} className={imageStyle} />
+      </Transition>
+    )
+  }
+  changeImage() {
+    const { gallery, currentImageIndex } = this.state;
+    this.setState({
+      currentImageIndex: currentImageIndex === (gallery.length - 1) ? 0 : currentImageIndex + 1
+    });
+    this.renderImage();
   }
   render() {
-    let { currentImage } = this.state;
     return (
-      <div className={viewer}>
-        {/* <Img sizes={this.state.currentImage.sizes} /> */}
-      </div>
+      <TransitionGroup className={viewer} onClick={() => this.changeImage()}>
+        {this.renderImage()}
+      </TransitionGroup>
     )
   }
 }
@@ -34,5 +60,9 @@ const viewer = css`
   height: 100%;
   width: 100%;
   /* background: red; */
+  /* display: inline-block; */
   overflow: hidden;
+`
+const imageStyle = css`
+  /* display: inline; */
 `
