7ed8c05eca17a8b80a8108e773841805892449ce
zackcreach <zackcreach@gmail.com>
2017-11-28 14:43:30 -0500

    Settled for fade in out in lieu of duplication.

--- src/layouts/viewer.js
+++ src/layouts/viewer.js
@@ -18,37 +18,34 @@ class Viewer extends React.Component {
     }
   }
   componentWillMount() {
-    // setInterval(() => this.changeImage(), 2000);
+    setInterval(() => this.changeImage(), 6000);
+  }
+  changeImage() {
+    this.setState({
+      show: !this.state.show,
+    });
   }
   renderImage() {
     const { gallery, currentImageIndex, show } = this.state;
     return (
-      <Img sizes={gallery[currentImageIndex].sizes} key={gallery[currentImageIndex].sizes.src} className={imageStyle} />
+      <Img sizes={gallery[currentImageIndex].sizes} key={Date.now()} className={imageStyle} />
     )
   }
   render() {
     const { gallery, currentImageIndex, show } = this.state;
-    console.log(gallery[currentImageIndex].sizes.src);
-    
     return (
-      <div className={viewer} onClick={() => {
-        this.setState({
-          show: !this.state.show,
-        });
-      }}>
-        <CSSTransition
-          timeout={1000}
-          classNames='slide'
+      <div className={viewer}>
+        <Slide
           in={show}
           onExited={() => (
             this.setState({ 
-              show: !this.state.show,
+              show: !show,
               currentImageIndex: currentImageIndex === (gallery.length - 1) ? 0 : currentImageIndex + 1,
             })
           )}
         >
           {this.renderImage()}
-        </CSSTransition>
+        </Slide>
       </div>
     )
   }
@@ -56,63 +53,21 @@ class Viewer extends React.Component {
 
 export default Viewer;
 
+const Slide = ({ ...props, children }) => (
+  <CSSTransition
+    {...props}
+    timeout={800}
+    classNames='slide'
+  >
+    {children}
+  </CSSTransition>
+)
+
 const viewer = css`
   height: 100%;
   width: 100%;
-  /* background: red; */
-  /* display: inline-block; */
+  background: var(--turq);
   overflow: hidden;
 `
 const imageStyle = css`
-  /* display: inline; */
 `
-
-
-// Map method
-
-// class Viewer extends React.Component {
-//   constructor({ imageData }) {
-//     super();
-//     this.state = {
-//       gallery: Object.values(imageData),
-//       currentImageIndex: 0,
-//       show: true
-//     }
-//   }
-//   componentWillMount() {
-//     // setInterval(() => this.changeImage(), 2000);
-//   }
-//   renderImage() {
-//     const { gallery, currentImageIndex, show } = this.state;
-//     // show ? <p key={1}>TRUE</p> : <p key={2}>FALSE</p>
-//     return (
-//       gallery.map((image, i) => (
-//         <CSSTransition
-//           key={i} 
-//           timeout={1000}
-//           classNames='slide'
-//           in={show}
-//         >
-//           <Img sizes={image.sizes} className={imageStyle} position='absolute' />
-//         </CSSTransition>
-//       ))
-//     )
-
-//   }
-//   changeImage() {
-//     const { gallery, currentImageIndex, show } = this.state;
-//     this.setState({
-//       show: !show,
-//     });
-//   }
-//   render() {
-//     const { gallery, currentImageIndex, show } = this.state;
-//     return (
-//       <div className={viewer} onClick={() => this.changeImage()}>
-//         <TransitionGroup>
-//           {this.renderImage()}
-//         </TransitionGroup>
-//       </div>
-//     )
-//   }
-// }
