674b8abd618438760e33431f0b84f5cccd0a445c
zackcreach <zackcreach@gmail.com>
2017-11-28 15:04:28 -0500

    Updated 404 page to match others.

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,40 @@
 import React from 'react';
 
-const NotFound = () => (
-  <div>
-    <h1>Oops!</h1>
-    <p>Page not found.</p>
-  </div>
-)
+import '../styles/styles.scss';
+import styled, { css } from 'emotion';
 
+import Header from '../layouts/header';
+import Content from '../layouts/content';
+import Transition from '../components/transition';
+
+class NotFound extends React.Component {
+  state = {
+    width: null,
+    in: true
+  }
+  componentDidMount() {
+    this.setState({
+      width: this.container.offsetWidth
+    })
+  }
+  componentWillUnmount() {
+    this.setState({
+      in: false
+    })
+  }
+  render() {
+    return (
+      <section>
+        <Header headerWidth={this.state.width} />
+        <Transition show={this.state.in}>
+          <div ref={node => this.container = node}>
+            <Content>
+              <h2>Oops! Page Not Found.</h2>
+            </Content>
+          </div>
+        </Transition>
+      </section>
+    )
+  }
+}
 export default NotFound;
