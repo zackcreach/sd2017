3f710821a29b71d0614995353248cbc00c200b1e
Zack Creach <zackcreach@gmail.com>
2018-02-27 22:02:35 -0500

    GraphCMS up and running!!!

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,13 +20,16 @@ module.exports = {
         endpoint: `https://api.graphcms.com/simple/v1/skylardenney`,
         token: `eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1MTk2OTc3NjUsImNsaWVudElkIjoiY2l2Z29zNmNqMDE5MjAxODRucDAxZGRkMiIsInByb2plY3RJZCI6ImNqZTM2azdpMzFtZmgwMTU5NnQxYWtmbWwiLCJwZXJtYW5lbnRBdXRoVG9rZW5JZCI6ImNqZTUwdDhiOTM5bXMwMTQ1MWtlZnF2c3EifQ.0z1bRHxNimVUzVAZ5TF1lSeUBALxCBSNBp6fJL9pxuU`,
         query: `{
-            allResumeItems {
-              id
-              type
-              name
-              role
-              credit
-            }
+          allResumes {
+            createdAt
+            updatedAt
+            isPublished
+            id
+            type
+            name
+            role
+            credit
+          }
         }`,
       },
     },
